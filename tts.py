406bf1b40216d4e79979cab8a950889cd609415c
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-10-01 00:12:14 +0900

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,4 +1,14 @@
-# EDGE TTS 쓸거양
-# 아니면 gtts 를 쓸거양
+import os
 
+try:
+  import edge_tts 
+  text = "안녕하세요. Edge TTS를 사용하여 텍스트를 음성으로 변환해 보겠습니다."
+  voice="ko-KR-SunHiNeural"
 
+  tts = edge_tts.Communicate(text, voice)
+
+  sound_file = os.path.join(os.getcwd(), 'tts', 'tts_generated.mp3')
+  tts.save_sync(sound_file)
+  
+except:
+  import gtts
