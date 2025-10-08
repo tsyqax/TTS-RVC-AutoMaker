8ac48cc7b8e9b0dc26c26bd8cbe5624931484df1
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-10-08 10:40:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def load_number():
 def save_number(number):
   try:
     with open('number.txt', 'w') as file:
-      int(number) += 1
+      number += 1
       file.write(str(number))
   except Exception as e:
      print(f"ERROR: {e}")
@@ -112,5 +112,5 @@ if __name__ == '__main__':
     os.makedirs(os.path.join(os.getcwd(), 'output', args.rvc_name), exist_ok=True)
     rvc_output_path = os.path.join(os.getcwd(), 'output', args.rvc_name, f'txt2sound_{number}.mp3')
     rvc_song(rvc_index_path, rvc_model_path, args.index_rate, rvc_input_path, rvc_output_path, pitch_vocal, args.rvc_method, 3, 0.8, 0.33, 128)
-    save_number(number)
+    save_number(int(number))
     print(f'DONE!! {number}!!')
